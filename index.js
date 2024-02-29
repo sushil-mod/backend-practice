996a9ca93c9c9d3db366ffa2e38a443d723794db
sushil-mod <78729065+sushil-mod@users.noreply.github.com>
2024-02-29 23:52:03 +0530

    First get request wiht query params

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
-function initialSetup (){
-    console.log("hello world");
-}
+const express = require("express");
+const port =8000;
+
+const app = express();
+
+app.get("/",(req,res)=>{
+    res.send({name:"sushil",age:24});
+})
+app.get("/add",(req,res)=>{
+    const {firstnum,secondnum} = req.query;
+    console.log("number",firstnum,secondnum)
+    let sum = Number(firstnum)+Number(secondnum);
+    res.send({sum});
+})
+app.listen(port,()=>console.log("listening on",port));
+
