44a319a0c7756744e3bc16a74c4837d9c8980f18
sushil-mod <78729065+sushil-mod@users.noreply.github.com>
2024-03-03 22:44:12 +0530

    Feat: added todo CURD api

--- index.js
+++ index.js
@@ -1,16 +1,40 @@
+
 const express = require("express");
+const bodyParser = require("body-parser");
 const port =8000;
 
 const app = express();
 
-app.get("/",(req,res)=>{
-    res.send({name:"sushil",age:24});
+app.use(bodyParser.json());
+
+let todos = []
+let counter = 1;
+
+app.get("/todos",(req,res)=>{
+    res.send({data:todos});
 })
-app.get("/add",(req,res)=>{
-    const {firstnum,secondnum} = req.query;
-    console.log("number",firstnum,secondnum)
-    let sum = Number(firstnum)+Number(secondnum);
-    res.send({sum});
+app.get("/todos/:id",(req,res)=>{
+    const {id} = req.params;
+    console.log("get /todo/:id",id);
+    let resposne = todos?.find((todo) => todo?.id === Number(id) )
+    res.send({data:resposne});
+
 })
+app.post("/todo",(req,res)=>{
+    const reqBody = req.body;
+    console.log("post /todo",reqBody);
+    let newTodo = {id:counter,...reqBody};
+    todos = [...todos,newTodo];
+    counter++ ;
+    res.send({data:newTodo});
+})
+
+app.delete("/todo/:id",(req,res)=>{
+    const {id} = req.query;
+    console.log("delete /todo/:id",reqBody);
+    todos = todos?.filter((todo) =>todo?.id === id);
+    res.send("todo deleted successfully");
+})
+
 app.listen(port,()=>console.log("listening on",port));
 
